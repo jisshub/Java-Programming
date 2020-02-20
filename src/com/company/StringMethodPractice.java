233eedd5d94a54f5eca63cafed1f68e748b295cb
jissmon jose <jissmon476@gmail.com>
2020-02-20 06:53:27 +0530

    String methods practice

--- src/com/company/StringMethodPractice.java
+++ src/com/company/StringMethodPractice.java
@@ -7,13 +7,13 @@ public class StringMethodPractice {
 //        largest sentence in paragaph
         String paragraph = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.";
         String[] array1 = paragraph.split("\\.");
+        System.out.println(Arrays.toString(array1));
 
-        for (String each: array1
-             ) {
-             String new1 = each.replace(" ", ",");
-
-            System.out.println(each.length());
+        for (int i = 0; i < array1.length ; i++) {
+            for (String eachSent: array1
+                 ) {
+//                if(Character.isLetter(eachSent.charAt(i)) && i != enfofLine)
+            }
         }
-
     }
 }
